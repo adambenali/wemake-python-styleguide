9b4f1b67f8e422bb3305363c90c673395cd9583c
Gabriel <gc.chang95@gmail.com>
2020-02-27 11:32:26 +0100

    Add tests for check_valid_shebang #7

---
+++ tests/test_visitors/test_tokenize/test_comments/test_valid_shebang.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+from pathlib import Path
+
+import pytest
+
+from wemake_python_styleguide.violations.best_practices import (
+    ExecutableMismatchViolation
+)
+from wemake_python_styleguide.visitors.tokenize.comments import (
+    FileMagicCommentsVisitor
+)
+
+_python_files_folder = Path(__file__).absolute().parent / 'test_valid_shebang_resources'
+
+@pytest.mark.parametrize("error_code", [
+    'exe001',
+    'exe002',
+    'exe003',
+    'exe004',
+    'exe005'])
+def test_exe_negative(
+    assert_errors,
+    parse_tokens,
+    default_options,
+    error_code
+):
+    """Testing cases when no errors should be reported"""
+    filename = _python_files_folder / (error_code + '_neg.py')
+    with open(filename) as f:
+        contents = f.read()
+        file_tokens = parse_tokens(contents)
+        visitor = FileMagicCommentsVisitor(default_options, filename=filename, file_tokens=file_tokens)
+        visitor.run()
+        assert_errors(visitor,[])
+
+
+@pytest.mark.parametrize("error_code", [
+    'exe001',
+    'exe002',
+    'exe003',
+    'exe004',
+    'exe005'])
+def test_exe_positive(
+    assert_errors,
+    parse_tokens,
+    default_options,
+    error_code
+):
+    """testing cases when errors should be reported"""
+    filename = _python_files_folder / (error_code + '_pos.py')
+    with open(filename) as f:
+        contents = f.read()
+        file_tokens = parse_tokens(contents)
+        visitor = FileMagicCommentsVisitor(default_options, filename=filename, file_tokens=file_tokens)
+        visitor.run()
+        assert_errors(visitor, [ExecutableMismatchViolation])
